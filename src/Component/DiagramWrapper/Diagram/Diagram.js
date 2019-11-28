bff6dc6542affe72b707128637c59f4ddf78788f
Beksultan Kadyrbekov <kbeksss@gmail.com>
2019-11-28 17:12:20 +0600

    added some styles to the diagram

--- src/Component/DiagramWrapper/Diagram/Diagram.js
+++ src/Component/DiagramWrapper/Diagram/Diagram.js
@@ -7,13 +7,13 @@ const Diagram = (props) => {
     let entertainmentPer = finances.filter(exp => (exp.type === 'entertainment')).reduce((total, exp) => (total + exp.cost), 0) * 100 / total;
     let carPer = finances.filter(exp => (exp.type === 'car')).reduce((total, exp) => (total + exp.cost), 0) * 100 / total;
     let foodPer = finances.filter(exp => (exp.type === 'food')).reduce((total, exp) => (total + exp.cost), 0) * 100 / total;
-    return(
+    return total ? (
         <div className='Diagram'>
-            <div className='Entertainment' style={{width: `${entertainmentPer}%`}}/>
-            <div className='Car' style={{width: `${carPer}%`}}/>
-            <div className='Food' style={{width: `${foodPer}%`}}/>
+            <div className='Entertainment' style={{width: `${entertainmentPer}%`}}><i>{Math.trunc(entertainmentPer * 100)/100} %</i></div>
+            <div className='Car' style={{width: `${carPer}%`}}><i>{Math.trunc(carPer * 100)/100} %</i></div>
+            <div className='Food' style={{width: `${foodPer}%`}}><i>{Math.trunc(foodPer * 100)/100} %</i></div>
         </div>
-    );
+    ) : null;
 };
 
 export default Diagram;
