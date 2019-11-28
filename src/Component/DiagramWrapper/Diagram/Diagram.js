57e890d8dbcbe6f570423e85b791c7090985a530
Beksultan Kadyrbekov <kbeksss@gmail.com>
2019-11-28 17:23:07 +0600

    deleted some useless variables

--- src/Component/DiagramWrapper/Diagram/Diagram.js
+++ src/Component/DiagramWrapper/Diagram/Diagram.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './Diagram.css';
 
-const Diagram = (props) => {
-    let finances = props.finances;
-    let entertainmentPer = finances.filter(exp => (exp.type === 'entertainment')).reduce((total, exp) => (total + exp.cost), 0) * 100 / props.total;
-    let carPer = finances.filter(exp => (exp.type === 'car')).reduce((total, exp) => (total + exp.cost), 0) * 100 / props.total;
-    let foodPer = finances.filter(exp => (exp.type === 'food')).reduce((total, exp) => (total + exp.cost), 0) * 100 / props.total;
-    return props.total ? (
+const Diagram = ({finances, total}) => {
+    let entertainmentPer = finances.filter(exp => (exp.type === 'entertainment')).reduce((total, exp) => (total + exp.cost), 0) * 100 / total;
+    let carPer = finances.filter(exp => (exp.type === 'car')).reduce((total, exp) => (total + exp.cost), 0) * 100 / total;
+    let foodPer = finances.filter(exp => (exp.type === 'food')).reduce((total, exp) => (total + exp.cost), 0) * 100/ total;
+    return total ? (
         <div className='Diagram'>
             <div className='Entertainment' style={{width: `${entertainmentPer}%`}}><i>{Math.trunc(entertainmentPer * 100)/100} %</i></div>
             <div className='Car' style={{width: `${carPer}%`}}><i>{Math.trunc(carPer * 100)/100} %</i></div>
